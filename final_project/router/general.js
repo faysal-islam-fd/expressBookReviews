ec96b7b294a9e692975e18a5c98cb9a42b96c431
faysal-islam-fd <fahadiislam01@gmail.com>
2024-10-21 22:59:43 -0700

    task 4 added

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -55,14 +55,22 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  const title = req.params?.title;
+  if(!title){
+    return res.status(400).json({message:"Please provide a title"})
+  }
+
+  const booksByTitle = Object.values(books).filter(book=> book.title === title)
+  if(booksByTitle.length!==0){
+    return res.status(200).json({booksByTitle})
+  }
+  res.status(404).json({message:"Books not found"})
+  });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
 });
 
 module.exports.general = public_users;
