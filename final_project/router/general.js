27d54a7df980b164bac26e866d7c5fb85b9c9ae3
faysal-islam-fd <fahadiislam01@gmail.com>
2024-10-21 22:47:25 -0700

    Task 3 implemtentation done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -35,8 +35,22 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  
+  if(author){
+   
+    books = Object.values(books)
+
+  const findAuthor =  books.find(book=> book.author.toLowerCase().includes(author.toLowerCase()))
+   
+    
+  if(findAuthor){
+    const booksByAuthor = books.filter(book=>book.author===findAuthor.author)
+    return res.status(200).json({booksByAuthor})
+  }
+    return res.status(404).json({message:"Author not found"})
+  }
+  return res.status(400).json({message:"Please provide a author name"})
 });
 
 // Get all books based on title
